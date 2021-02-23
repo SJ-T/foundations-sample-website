934d17dc3fe4961742a3ea775006b8f64fc0b0d5
Sijia Tang <sijia.tang@code.berlin>
2021-02-23 16:46:32 +0800

    change greeting

--- sample_website/website.py
+++ sample_website/website.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Flask!</h1>'
+    return '<h1>Hello, Nicoco!</h1>' 
 
-
+ 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
