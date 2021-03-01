7f6a332292021756cbe8a00d2628971ea2cef50e
Sijia Tang <sijia.tang@code.berlin>
2021-03-01 15:27:05 +0800

    bug in sample fixed

--- sample_website/website.py
+++ sample_website/website.py
@@ -4,13 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-    return '<h1>Hello prpr</h1>' 
-=======
     return '<h1>Hello, Class!</h1>'
 
->>>>>>> upstream/main
 
- 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
