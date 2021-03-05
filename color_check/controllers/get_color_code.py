ef88ce8906e3d764bbefe4ff2dcaba792401c341
Sijia Tang <sijia.tang@code.berlin>
2021-03-06 01:22:09 +0800

    back

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -17,8 +17,8 @@ def get_color_code(color_name):
         
         #hex_code = color.get(color_name)
     
-    if color_name.lower() in color_names:
-        hex_code = color[color_name.lower()]
+    if color_name in color_names:
+        hex_code = color[color_name]
         
         return hex_code
 
